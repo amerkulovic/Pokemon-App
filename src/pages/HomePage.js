f94671627810cfcd454207e7f8d63a72c6309bc8
Amer Kulovic <amer.kulovic@yahoo.com>
2023-06-05 11:35:12 -0500

    installed tailwinds and added move list section

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,22 +2,42 @@ import { useState, useEffect } from "react";
 
 const HomePage = () => {
   let [pokemon, setPokemon] = useState(null);
+  // let [pokemonMoves, setPokemonMoves] = useState(pokemon.moves);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+    fetch("https://pokeapi.co/api/v2/pokemon/raikou")
       .then((response) => response.json())
-      .then((data) => setPokemon(data));
+      .then((data) => {
+        console.log(data.sprites);
+        setPokemon(data);
+      });
   }, []);
 
   return (
-    <>
+    <div className="">
       {pokemon && (
         <div>
-          <h1>{pokemon.name}</h1>
-          <img src={pokemon.sprites.front_default} />
+          <section className="flex flex-col text-center justify-center px-4 border border-red-200">
+            <h1 className="font-bold text-3xl">{pokemon.name}</h1>
+            <img className="h-40 w-40 flex mx-auto justify-center" src={pokemon.sprites.front_default} />
+            {pokemon.types.map((type, index) => (
+              <div>
+                <h2>{type.type.name}</h2>
+                {/* <img src={type.type.url} /> */}
+              </div>
+            ))}
+          </section>
+          <div className="flex justify-center pt-3">
+            <h1 className="font-bold">Move List</h1>
+          </div>
+          <section className="flex flex-wrap justify-center text-center pt-3">
+            {pokemon.moves.map((move, index) => (
+              <h2 className="w-1/3">{move.move.name}</h2>
+            ))}
+          </section>
         </div>
       )}
-    </>
+    </div>
   );
 };
 
