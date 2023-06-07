967fda6bf3d307289aca7a66345f87d8ced00c8c
Amer Kulovic <amer.kulovic@yahoo.com>
2023-06-07 10:47:41 -0500

    fixed capitilization issue with names

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,14 +9,31 @@ const HomePage = () => {
   let [pageStyling, setPageStyling] = useState("");
   let [statStyling, setStatStyling] = useState("");
   let [moveStyling, setMoveStyling] = useState("");
+  let [total, setTotal] = useState(0);
+  let [name, setName] = useState("");
+
+  let generateTotalStat = (numbers) => {
+    total = 0;
+    numbers.stats.map((stat) => {
+      setTotal((total += stat.base_stat));
+    });
+  };
+
+  let capitlizeName = (pokemon) => {
+    let nameArray = pokemon.name.split("");
+    let restOfName = nameArray.slice(1).join("");
+    nameArray = nameArray[0].toUpperCase() + restOfName;
+    setName(nameArray);
+  };
 
   const searchButtonHandler = async () => {
     await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.types[0].type.name);
-
         setPokemon(data);
+
+        generateTotalStat(data);
+        capitlizeName(data);
 
         if (data.types[0].type.name === "psychic") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-pink-400 bg-pink-200 rounded-lg mx-2 mb-3");
@@ -72,8 +89,8 @@ const HomePage = () => {
           setMoveStyling("border-4 border-orange-500 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "ground") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-orange-400 bg-orange-100 rounded-lg mx-2 mb-3");
-          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-orange-400 rounded-lg mx-2")
-          setMoveStyling("border-4 border-orange-400 rounded-lg mt-6 mx-2")
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-orange-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-orange-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "flying") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-purple-200 bg-purple-100 rounded-lg mx-2 mb-3");
           setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-purple-200 rounded-lg mx-2");
@@ -101,7 +118,7 @@ const HomePage = () => {
       {pokemon && (
         <div className="flex items-center mb-3">
           <section className={pageStyling}>
-            <h1 className="font-bold text-4xl mt-3">{pokemon.name}</h1>
+            <h1 className="font-bold text-4xl mt-3">{name}</h1>
             <img className="h-80 w-80 flex mx-auto justify-center" src={pokemon.sprites.other["official-artwork"].front_default} />
 
             <section>
@@ -125,6 +142,10 @@ const HomePage = () => {
                   <h2>{stat.base_stat}</h2>
                 </div>
               ))}
+              <div className="flex flex-row justify-between w-2/3 border-t-2 border-black mt-1">
+                <h2 className="font-bold text-xl">Total</h2>
+                <h2 className="font-bold text-xl">{total}</h2>
+              </div>
             </section>
             <div className={moveStyling}>
               <div className="flex justify-center pt-3">
