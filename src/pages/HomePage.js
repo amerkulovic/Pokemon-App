64511fa1b94fc85aed8ae1e3f7c211de7d7eec3e
Amer Kulovic <amer.kulovic@yahoo.com>
2023-06-07 09:47:19 -0500

    added more dynamic styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,12 +7,8 @@ const HomePage = () => {
   let [pokemon, setPokemon] = useState(null);
   let [input, setInput] = useState("");
   let [pageStyling, setPageStyling] = useState("");
-
-  // const editStyling = () => {
-  //   if (pokemon.types.type.name === "psychic") {
-  //     setPageStyling("bg-pink-200");
-  //   }
-  // };
+  let [statStyling, setStatStyling] = useState("");
+  let [moveStyling, setMoveStyling] = useState("");
 
   const searchButtonHandler = async () => {
     await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
@@ -24,40 +20,74 @@ const HomePage = () => {
 
         if (data.types[0].type.name === "psychic") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-pink-400 bg-pink-200 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-pink-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-pink-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "water") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-blue-400 bg-blue-200 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-blue-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-blue-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "grass") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-green-400 bg-green-200 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-green-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-green-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "dragon") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-purple-400 bg-purple-300 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-purple-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-purple-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "poison") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-purple-400 bg-purple-200 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-purple-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-purple-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "electric") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-yellow-400 bg-yellow-200 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-yellow-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-yellow-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "bug") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-green-400 bg-green-100 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-green-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-green-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "fire") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-red-400 bg-red-200 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-red-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-red-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "dark") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-black bg-slate-400 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-black rounded-lg mx-2");
+          setMoveStyling("border-4 border-black rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "fairy") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-pink-500 bg-pink-100 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-pink-500 rounded-lg mx-2");
+          setMoveStyling("border-4 border-pink-500 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "normal") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-slate-400 bg-slate-100 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-slate-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-slate-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "ice") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-blue-500 bg-blue-100 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-blue-500 rounded-lg mx-2");
+          setMoveStyling("border-4 border-blue-500 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "fighting") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-orange-500 bg-orange-200 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-orange-500 rounded-lg mx-2");
+          setMoveStyling("border-4 border-orange-500 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "ground") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-orange-400 bg-orange-100 rounded-lg mx-2 mb-3");
         } else if (data.types[0].type.name === "flying") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-purple-200 bg-purple-100 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-purple-200 rounded-lg mx-2");
+          setMoveStyling("border-4 border-purple-200 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "rock") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-orange-500 bg-orange-300 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-orange-500 rounded-lg mx-2");
+          setMoveStyling("border-4 border-orange-500 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "ghost") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-purple-500 bg-purple-300 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-pruple-400 rounded-lg mx-2");
+          setMoveStyling("border-4 border-pruple-400 rounded-lg mt-6 mx-2");
         } else if (data.types[0].type.name === "steel") {
           setPageStyling("flex flex-col text-center justify-center px-4 w-2/5 border-4 border-4 border-slate-500 bg-slate-300 rounded-lg mx-2 mb-3");
+          setStatStyling("flex flex-wrap flex-col items-center py-3 border-4 border-slate-500 rounded-lg mx-2");
+          setMoveStyling("border-4 border-slate-500 rounded-lg mt-6 mx-2");
         }
       });
   };
@@ -85,7 +115,7 @@ const HomePage = () => {
             <Sprite image1={pokemon.sprites.front_default} image2={pokemon.sprites.back_default} />
           </section>
           <div className="flex flex-col w-3/5">
-            <section className="flex flex-wrap flex-col items-center py-3 border-4 border-blue-400 rounded-lg mx-2">
+            <section className={statStyling}>
               <h1 className="flex text-center font-bold text-2xl pb-2">Stats</h1>
               {pokemon.stats.map((stat) => (
                 <div key={stat.stat.name} className="flex flex-row justify-between w-2/3">
@@ -94,7 +124,7 @@ const HomePage = () => {
                 </div>
               ))}
             </section>
-            <div className="border-4 border-blue-400 rounded-lg mt-6 mx-2">
+            <div className={moveStyling}>
               <div className="flex justify-center pt-3">
                 <h1 className="font-bold text-2xl pb-2">Move List</h1>
               </div>
